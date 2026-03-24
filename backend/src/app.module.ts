df3cf389af4f6b49ff3ffdb4e8b2734df6e2688d
divineamunega <divineamunega@gmail.com>
2026-03-24 11:23:12 +0100

    chore: clean up empty code change sections in the changes log

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { LoggerModule } from './logger/logger.module';
+import { AppService } from './app.service.js';
+import { AppController } from './app.controller.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { LoggerModule } from './logger/logger.module.js';
 
 @Module({
   imports: [PrismaModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,17 +1,26 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { LoggerService } from '../logger/logger.service.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
   constructor(private readonly logger: LoggerService) {
-    super()
+    super({ adapter })
   }
   async onModuleInit() {
-    await this.$connect();
-    this.logger.info("Prisma Connected to the database.")
+    try {
+      await this.$connect();
+      this.logger.info("Prisma Connected to the database.")
+
+    } catch (err) {
+      this.logger.error("An error occured while connecting to the database.", err)
+    }
   }
 
   async onModuleDestroy() {
